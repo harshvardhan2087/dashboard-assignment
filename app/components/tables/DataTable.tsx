2c361cdb3387027144a982ecaf8d3f741d500405
harshvardhan2087 <harshvardhanyadav2087@gmail.com>
2024-08-09 16:35:24 +0530

    fixed the bug

--- app/components/tables/DataTable.tsx
+++ app/components/tables/DataTable.tsx
@@ -12,43 +12,43 @@ type Props = {
     slug: string;
 };
 
-const DataTable = (props: Props) => {
-    const columns = Array.isArray(props.columns) ? props.columns : [];
-
+const DataTable = ({columns, rows}: Props) => {
     const actionColumn: GridColDef = {
         field: "action",
         headerName: "Action",
         width: 150,
-        renderCell: (params) => {
-            return (
-                <div className="flex gap-4 items-center">
-                        <Image src="/view.svg" alt="View" className="w-5 h-5 cursor-pointer" width={100} height={100} />
-                    <div className="cursor-pointer">
-                        <Image src="/delete.svg" alt="Delete" className="w-5 h-5" width={100} height={100} />
-                    </div>
+        renderCell: (params) => (
+            <div className="flex gap-4 items-center">
+                <Image
+                    src="/view.svg"
+                    alt="View"
+                    className="w-5 h-5 cursor-pointer"
+                    width={20}
+                    height={20}
+                />
+                <div className="cursor-pointer">
+                    <Image
+                        src="/delete.svg"
+                        alt="Delete"
+                        className="w-5 h-5"
+                        width={20}
+                        height={20}
+                    />
                 </div>
-            );
-        },
+            </div>
+        ),
     };
 
     return (
         <div className="p-5 bg-white rounded-lg shadow-md">
             <DataGrid
-                className="bg-white"
-                rows={props.rows}
+                rows={rows}
                 columns={[...columns, actionColumn]}
                 initialState={{
                     pagination: {
                         paginationModel: {
                             pageSize: 10,
                         },
-                    },
-                }}
-                components={{ Toolbar: GridToolbar }}
-                componentsProps={{
-                    toolbar: {
-                        showQuickFilter: true,
-                        quickFilterProps: { debounceMs: 500 },
                     },
                 }}
                 pageSizeOptions={[10]}
@@ -63,3 +63,82 @@ const DataTable = (props: Props) => {
 };
 
 export default DataTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+// import {
+//     DataGrid,
+//     GridColDef,
+//     GridToolbar,
+// } from "@mui/x-data-grid";
+// import Image from "next/image";
+
+// type Props = {
+//     columns: GridColDef[];
+//     rows: object[];
+//     slug: string;
+// };
+
+// const DataTable = (props: Props) => {
+//     const columns = Array.isArray(props.columns) ? props.columns : [];
+
+//     const actionColumn: GridColDef = {
+//         field: "action",
+//         headerName: "Action",
+//         width: 150,
+//         renderCell: (params) => {
+//             return (
+//                 <div className="flex gap-4 items-center">
+//                         <Image src="/view.svg" alt="View" className="w-5 h-5 cursor-pointer" width={100} height={100} />
+//                     <div className="cursor-pointer">
+//                         <Image src="/delete.svg" alt="Delete" className="w-5 h-5" width={100} height={100} />
+//                     </div>
+//                 </div>
+//             );
+//         },
+//     };
+
+//     return (
+//         <div className="p-5 bg-white rounded-lg shadow-md">
+//             <DataGrid
+//                 className="bg-white"
+//                 rows={props.rows}
+//                 columns={[...columns, actionColumn]}
+//                 initialState={{
+//                     pagination: {
+//                         paginationModel: {
+//                             pageSize: 10,
+//                         },
+//                     },
+//                 }}
+//                 components={{ Toolbar: GridToolbar }}
+//                 componentsProps={{
+//                     toolbar: {
+//                         showQuickFilter: true,
+//                         quickFilterProps: { debounceMs: 500 },
+//                     },
+//                 }}
+//                 pageSizeOptions={[10]}
+//                 checkboxSelection
+//                 disableRowSelectionOnClick
+//                 disableColumnFilter
+//                 disableDensitySelector
+//                 disableColumnSelector
+//             />
+//         </div>
+//     );
+// };
+
+// export default DataTable;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { BrowserRouter } from "react-router-dom";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
